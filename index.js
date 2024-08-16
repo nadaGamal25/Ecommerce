9cbda5b003d0b0c6a86ea3aa64114ff095654a85
nadaGamal25 <gnada1221@gmail.com>
2024-08-16 19:14:16 +0200

    stripe added

--- index.js
+++ index.js
@@ -7,10 +7,39 @@ import { globalError } from './src/middleware/globalError.js'
 import dotenv from 'dotenv';
 import { bootstrap } from './src/modules/bootstrap.js'
 import cors from 'cors'
+import Stripe from 'stripe';
+const stripe = new Stripe('sk_test_51PlTDERxvZKhUqen9YyDNkkPlTm82kW9edWjYo5VTQ6PizGEvzEENRC9RkRQ7aourKQxXoLZ7uma0rD54adtMVEY00bim6E52c');
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app =express()
+
+app.post('api/webhook', express.raw({type: 'application/json'}), (request, response) => {
+    const sig = request.headers['stripe-signature'].toString();
+  
+    let event;
+  
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, "whsec_Rl4mCuMeQVV0NYjAOGTIlp4nBUKixAQh");
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+    // Handle the event
+    let checkoutSessionCompleted 
+    switch (event.type) {
+      case 'checkout.session.completed':
+         checkoutSessionCompleted = event.data.object;
+        // Then define and call a function to handle the event checkout.session.completed
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  
+    // Return a 200 response to acknowledge receipt of the event
+    response.json({message:"success",checkoutSessionCompleted});
+  });
 app.use(cors())
 app.use(express.json())
 
